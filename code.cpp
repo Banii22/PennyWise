dd3f35a0e40ccc9a7f1d6a6c1f81ba1a2c6fbc0c
Banii22 <b.dhakal.2649@westcliff.edu>
2026-08-16 13:17:43 +0545

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+
+// This class represents ONE expense.
+class Expense
+{
+public:
+
+    int id;
+    string name;
+    string category;
+    double amount;
