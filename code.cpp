b05c237d0448633861adbf8ea584ff40a186f870
Banii22 <b.dhakal.2649@westcliff.edu>
2026-08-16 13:58:08 +0545

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -14,3 +14,16 @@ public:
     string name;
     string category;
     double amount;
+    
+    // This creates an expense and stores its information.
+    Expense(int i, string n, string c, double a)
+    {
+        id = i;
+        name = n;
+        category = c;
+        amount = a;
+    }
+};
+
+
+
