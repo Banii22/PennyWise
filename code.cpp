669e50aa6765a5106f293c23084cb8b1d0db3f6b
Banii22 <b.dhakal.2649@westcliff.edu>
2026-08-16 14:15:52 +0545

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,29 +1,17 @@
 #include <iostream>
-#include <vector>
 #include <string>
 
 using namespace std;
 
 
-// This class represents ONE expense.
+// This class represents one expense.
 class Expense
 {
 public:
-
     int id;
     string name;
     string category;
     double amount;
-    
-    // This creates an expense and stores its information.
-    Expense(int i, string n, string c, double a)
-    {
-        id = i;
-        name = n;
-        category = c;
-        amount = a;
-    }
 };
-
 
 
